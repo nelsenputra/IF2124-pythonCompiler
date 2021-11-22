1a207ceb7f8e86da050e714bfb2cb527aec8d239
ignferry <ignasius.ferry01@gmail.com>
2021-11-22 19:01:05 +0700

    fix cfg

--- CFG2CNF.py
+++ CFG2CNF.py
@@ -45,10 +45,10 @@ variablesJar = ['a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'a9', 'a10', 'a1
 'a1924', 'a1925', 'a1926', 'a1927', 'a1928', 'a1929', 'a1930', 'a1931', 'a1932', 'a1933', 'a1934', 'a1935', 'a1936', 'a1937', 'a1938', 'a1939', 'a1940', 'a1941', 'a1942', 'a1943', 'a1944', 'a1945', 'a1946', 'a1947', 'a1948', 'a1949', 'a1950', 'a1951', 'a1952', 'a1953', 
 'a1954', 'a1955', 'a1956', 'a1957', 'a1958', 'a1959', 'a1960', 'a1961', 'a1962', 'a1963', 'a1964', 'a1965', 'a1966', 'a1967', 'a1968', 'a1969', 'a1970', 'a1971', 'a1972', 'a1973', 'a1974', 'a1975', 'a1976', 'a1977', 'a1978', 'a1979', 'a1980', 'a1981', 'a1982', 'a1983', 
 'a1984', 'a1985', 'a1986', 'a1987', 'a1988', 'a1989', 'a1990', 'a1991', 'a1992', 'a1993', 'a1994', 'a1995', 'a1996', 'a1997', 'a1998', 'a1999', 'S', 'InProgram', 'Letter', 'Number', 'OtherChar', 'AnyChar', 'AnyString', 'Numbers', 'FloatNumbers', 'Boolean', 'EOL', 
-'ArithmeticOp', 'AssignmentOp', 'ComparisonOp', 'LogicalOp', 'IdentityOp', 'MembershipOp', 'BitwiseOp', 'Operator', 'Space', 'SpaceOrEmpty', 'IdentifierName', 'IdentifierName2', 'Variable', 'MultivarRight', 'Assignment', 'Value', 'OpParValue', 'OperatedValue', 
-'List', 'Tuple', 'Set', 'Dictionary', 'DictionaryContents', 'Contents', 'Class', 'ClassHead', 'ClassContents', 'Function', 'FunctionHead', 'Arguments', 'Argument', 'FunctionContents', 'InFunction', 'Return', 'FunctionCall', 'Loop', 'ForLoopHead', 'Iterator', 
-'WhileLoopHead', 'Conditions', 'LoopContents', 'InLoop', 'Break', 'Continue', 'LoopInFunc', 'FuncLoopContents', 'InFuncLoop', 'Import', 'ImportOptionalAs', 'With', 'WithHead', 'WithContents', 'WithInLoop', 'WithInFunc', 'WithInFuncLoop', 'If', 'Elif', 'Else', 
-'IfHead', 'ElifHead', 'ElseHead', 'IfContents', 'IfInLoop', 'ElifInLoop', 'ElseInLoop', 'IfInFuncLoop', 'ElifInFuncLoop', 'ElseInFuncLoop', 'Ternary0', 'Ternary', 'Statement', 'Statement', 'NoEOL', 'SinglelineComment', 'MultilineComment']
+'ArithmeticOp', 'AssignmentOp', 'ComparisonOp', 'LogicalOp', 'IdentityOp', 'MembershipOp', 'BitwiseOp', 'Operator', 'Space', 'SpaceOrEmpty', 'IdentifierName', 'IdentifierName2', 'Variable', 'MultivarRight', 'Assignment', 'Value', 'OpParValue', 'OperatedValue', 'List', 
+'Tuple', 'Set', 'Dictionary', 'DictionaryContents', 'Contents', 'Class', 'ClassHead', 'ClassContents', 'Function', 'FunctionHead', 'Arguments', 'Argument', 'FunctionContents', 'InFunction', 'Return', 'FunctionCall', 'Loop', 'ForLoopHead', 'Iterator', 'WhileLoopHead', 
+'Conditions', 'LoopContents', 'InLoop', 'Break', 'Continue', 'LoopInFunc', 'FuncLoopContents', 'InFuncLoop', 'Import', 'ImportOptionalAs', 'With', 'WithHead', 'WithContents', 'WithInLoop', 'WithInFunc', 'WithInFuncLoop', 'If', 'Elif', 'Else', 'IfHead', 'ElifHead', 
+'ElseHead', 'IfContents', 'IfInLoop', 'ElifInLoop', 'ElseInLoop', 'IfInFuncLoop', 'ElifInFuncLoop', 'ElseInFuncLoop', 'Ternary0', 'Ternary', 'Statement', 'StatementNoEOL', 'SinglelineComment', 'MultilineComment']
 
 
 def isUnitary(rule, variables):
