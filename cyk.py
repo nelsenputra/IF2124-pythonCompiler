4d21aa05dce5c818fc664e4037a0537045595e2e
ignferry <ignasius.ferry01@gmail.com>
2021-11-23 19:51:46 +0700

    memperbaiki cyk dan cfg, Menambahkan FA untuk deteksi nama identifier

--- cyk.py
+++ cyk.py
@@ -46,12 +46,12 @@ def read_cnf():
 
 
 # Read input
-def read_input():
+def read_input(filename):
     """
     Membaca file input.py di folder yang sama dan mengubahnya menjadi sebuah string
     """
 
-    filename = os.path.join(os.curdir, "./test/raise.py")
+    filename = os.path.join(os.curdir, ("./test/" + filename))
     with open(filename) as input_file:
         lines = input_file.readlines()
         input_string = ''
@@ -71,42 +71,102 @@ def make_pairs(set_a, set_b):
 
     return pairs_list
 
+# FA
+def is_possible_variable(input_string):
+    """
+    Finite Automata untuk menentukan apakah suatu frase merupakan variabel yang valid namanya
+    """
+    keywords = ['False', 'class', 'is', 'return', 'None', 'continue', 'for', 'True', 'def', 'from', 'while', 'and', 'not', 'with', 'as', 'elif', 'if', 'or', 'else', 'import', 'pass', 'break', 'in', 'raise']
+    Alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    Numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-# Cyk
-def make_cyk_table(v_rules, t_rules, input_string):
+    if input_string in keywords:
+        # String merupakan salah satu reserved keywords
+        return False
+    else:
+        # State (S: Start state, A: Accepting state, D: Dead state)
+        currentState = 'S'
+
+        for i in input_string:
+            if currentState == 'S':
+                if i in Alphabets or i == '_':
+                    currentState = 'A'
+                else:
+                    currentState = 'D'
+            elif currentState == 'A':
+                if not(i in Alphabets or i in Numbers or i == '_'):
+                    currentState = 'D'
+                # Jika termasuk alphabet, angka, atau '_' tidak berubah state
+            else:
+                break
+
+        if currentState == 'A' :   
+            return True
+        else:
+            return False
+
+
+def checkForIdentifierNames(v_rules, input_string, table):
+
+    IdentifierRules = set()
+    for rules in v_rules:
+        if rules[1] == ['IdentifierName']:
+            IdentifierRules.add(rules[0])
+    IdentifierRules.add('IdentifierName')
+
+    for i in range(len(input_string)-1,-1,-1):
+        for j in range(len(input_string)-i):
+            if len(table[i][j]) != 1:
+                if is_possible_variable(input_string[j:j+i+1]):
+                    table[i][j] = IdentifierRules
+                    if i != 0:
+                        for k in range(i):
+                            for l in range(j, j+i+1):
+                                table[k][l] = {'d'}
+                        
+    for i in range(len(input_string)):
+        for j in range(len(input_string)-i):
+            if len(table[i][j]) == 1:
+                table[i][j] = set()
+
+    return table
+
+# CYK
+def process_cyk_table(v_rules, t_rules, input_string, table):
     string_length = len(input_string)
-
-    # Buat tabel
-    table = [[set() for _ in range(string_length - i)] for i in range(string_length)]
 
     # Proses baris pertama tabel (terminal symbol)
     for i in range(string_length):
         for rules in t_rules:
             if input_string[i] == rules[1]:
                 table[0][i].add(rules[0])
+        print(table[0][i])
     
+    print("\n\n")
     # Proses sisa baris pada tabel (non-terminal symbol)
     for i in range(1, string_length):
-        for j in range(0,string_length-i):
+        for j in range(string_length-i):
             for k in range(i):
                 pairs = make_pairs(table[k][j], table[i-k-1][j+k+1])
                 for rules in v_rules:
                     if rules[1] in pairs:
                         table[i][j].add(rules[0])
-
-    for i in table:
-        for j in i:
-            print(j)
-        print("\n")
-
+            print(table[i][j])
+        print("\n\n")
     return table
 
 
 if __name__ == '__main__':
     v_rules, t_rules = read_cnf()
-    input_string = read_input()
+    filename = input("Masukkan nama file: ")
+    input_string = read_input(filename)
     print([input_string])
-    table = make_cyk_table(v_rules, t_rules, input_string)
+    
+    # Buat tabel
+    table = [[set() for _ in range(len(input_string) - i)] for i in range(len(input_string))]
+    table = checkForIdentifierNames(v_rules, input_string, table)
+    table = process_cyk_table(v_rules, t_rules, input_string, table)
+
     if len(input_string) > 0:
         if 'S0' in table[len(input_string)-1][0]:
             print("Accepted!")
