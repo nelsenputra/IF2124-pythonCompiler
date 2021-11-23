d7bf37741db1dea4af3930468e696af61759da49
Johannes <johannessukiatmodjo20@gmail.com>
2021-11-23 21:57:20 +0700

    test terus

--- test/array.py
+++
@@ -1,3 +0,0 @@
-arr = [0 for i in range(10)]
-for i in range(10):
-    print(arr[i], end=" ")
---
+++ test/pass_raise.py
@@ -0,0 +1,10 @@
+for i in range(5):
+    pass
+
+class Contoh:
+    pass
+
+def hitung_luas_lingkaran(diameter):
+    pass
+
+raise hitung_luas_lingkaran(7)
